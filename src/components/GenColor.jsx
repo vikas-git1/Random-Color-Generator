c54999149a70a4fb5c57e1fb76c02766640562fd
vikas-git1 <vikasmewara.dev@gmail.com>
2024-08-28 06:51:46 +0530

    Add logic to generate the random color code and display it on the browser

--- src/components/GenColor.jsx
+++ src/components/GenColor.jsx
@@ -1,15 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./GenColor.css";
 const GenColor = () => {
-  //   const [color, setColor] = useState("black");
+  const [color, setColor] = useState("black");
+  const [colorCode, setColorCode] = useState("");
+  const colorCodeRef = useRef(null);
+  let chars = "1234567890ABCDEF";
+
+  const handleGenerateColor = () => {
+    let randomColor = "#";
+    for (let i = 0; i < 6; i++) {
+      let randomIndex = Math.floor(Math.random() * chars.length);
+      randomColor += chars.charAt(randomIndex);
+    }
+
+    setColor(randomColor);
+    setColorCode(randomColor);
+  };
+
+  const handleCopyPass = () => {
+    colorCodeRef.current.select();
+
+    navigator.clipboard
+      .writeText(colorCode)
+      .then(() => alert(`Color Code: ${colorCode} copied`));
+  };
   return (
     <>
-      {" "}
       <div className="container" style={{ backgroundColor: color }}>
-        <button className="btn genColor">Generate Color</button>
-        <button className="btn copyColor">Copy Color</button>
+        <button className="btn genColor" onClick={handleGenerateColor}>
+          Generate Color
+        </button>
+        <button className="btn copyColor" onClick={handleCopyPass}>
+          Copy Color
+        </button>
       </div>
-      <input type="text" className="colorCode" />
+      {/* <div className="colorCode">{colorCode}</div> */}
+      <input
+        type="text"
+        className="colorCode"
+        value={colorCode}
+        ref={colorCodeRef}
+        style={{ backgroundColor: color }}
+        readOnly
+      />
     </>
   );
 };
